cabba39bfeaaa6db0f7d935b62cffc665bb3e292
Ivan Castro <73139491+ivancastro87970@users.noreply.github.com>
2021-03-12 16:58:58 +0000

    Update main.cpp

--- desafio3/main.cpp
+++ desafio3/main.cpp
@@ -6,14 +6,30 @@ using namespace std;
 
 int main() 
 {
+    /* -- Se for por posição da lista --
+    
     list <int> li = { 1,2,3,4,5,6,7,8,9,10};
 
-    list<int>::iterator it = next(li.begin(), 2); // vai remover o da posi��o 2, isto � o '3'.
+    list<int>::iterator it = next(li.begin(), 2); // vai remover o da posição 2, isto é o '3'.
 
     li.erase(it); // apaga o que o iterador aponta.
 
     for (int& i : li) 
     {
         cout << i << endl;    //iteram as referencias da lista
+    }*/
+    
+    // -- Se for por nr a remover --
+    list <int> li = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    for (list<int>::iterator it = li.begin(); it != li.end(); it++)
+    {
+        if (*it == 2) // vai remover o valor que quero, neste caso é '2'.
+            it = li.erase(it);
+    }
+
+    for (int& i : li) // iteram as referencias da lista.
+    {
+        cout << i << endl;
     }
 }
